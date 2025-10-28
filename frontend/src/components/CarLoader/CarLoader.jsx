3c403c81ab272cc24f42396fd5f71f9ec00a4d60
unknown <surekha@pengwintech.com>
2025-10-28 21:03:08 +0530

    made changes in carLoaderanimation path in carLoader

--- frontend/src/components/CarLoader/CarLoader.jsx
+++ frontend/src/components/CarLoader/CarLoader.jsx
@@ -1,16 +1,16 @@
 import React from "react";
 import Lottie from "lottie-react";
-import carLoaderAnimation from "../../animations/carloaderanimation.json";
+import carLoaderAnimation from "../../animations/carLoaderanimation.json";
 import styles from "./CarLoader.module.scss";
 
 const CarLoader = () => {
   return (
     <div className={styles.lottieWrapper}>
-      <Lottie 
-        animationData={carLoaderAnimation} 
-        loop={true} 
-        autoplay={true} 
-        className={styles.lottie} 
+      <Lottie
+        animationData={carLoaderAnimation}
+        loop={true}
+        autoplay={true}
+        className={styles.lottie}
       />
     </div>
   );
